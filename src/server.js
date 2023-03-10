e4fab6a78315ae489e144cd39d1cd0be591ad906
Maksim Popov <makso16108@gmail.com>
2023-03-10 23:43:50 +0300

    Настроена раздача статики.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 3000;
 
 app.use(express.static(__dirname + '/../dist'));
 
+app.get('/*', (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/../dist/"});
+})
+
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 })
